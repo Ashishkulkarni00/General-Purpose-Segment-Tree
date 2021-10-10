7ce2662edc61923f94527d33c9458df53efd8939
Ashish Kulkarni <80530827+Ashishkulkarni00@users.noreply.github.com>
2021-10-10 12:23:31 +0530

    Add files via upload

---
+++ src/segment_tree/TreeNodeUse.java
@@ -0,0 +1,13 @@
+package segment_tree;
+
+public class TreeNodeUse {
+
+	public static void main (String args[]){
+		
+		Segment obj = new Segment();
+		System.out.println(obj.getSum(1, 3));
+	}
+	
+}
+
+
